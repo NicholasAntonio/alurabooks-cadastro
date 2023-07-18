1863a2f3c5ae4acd27a4c0a4c905f3d3ebaeae93
“NicholasAntonio” <nicholas.nkt.antonio@gmail.com>
2023-07-17 22:57:42 -0300

    final commit

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
-const consultaCEP = fetch('https://viacep.com.br/ws/01001000/json/');
+async function buscaEndereco(cep) {
+  var mensagemErro = document.getElementById("erro");
+  mensagemErro.innerHTML = "";
+  try {
+    const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+    const consultaCEPConvertida = await consultaCEP.json();
+    if (consultaCEPConvertida.erro) {
+      throw Error("CEP inexistente!");
+    }
+    var cidade = document.getElementById("cidade");
+    var logradouro = document.getElementById("endereco");
+    var estado = document.getElementById("estado");
+    var bairro = document.getElementById("bairro");
+    bairro.value = consultaCEPConvertida.bairro;
+    cidade.value = consultaCEPConvertida.localidade;
+    logradouro.value = consultaCEPConvertida.logradouro;
+    estado.value = consultaCEPConvertida.uf;
+    console.log(consultaCEPConvertida);
+    return consultaCEPConvertida;
+  } catch (erro) {
+    console.log(erro);
+    mensagemErro.innerHTML = `<p>CEP inválido. Tente novamente! </p>`;
+  }
+}
+
+var cep = document.getElementById("cep");
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
